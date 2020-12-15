386aea11503e1c3e43101ff74cfcec61855f69a5
Steven Woodward <steveanthony999@gmail.com>
2020-12-14 19:20:27 -0800

    styled home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div id='home'>
-      <p>Welcome to</p>
-      <h1>
-        Nav<span>Bam</span>
-      </h1>
-      <Link to={'builder'} className='btn'>
-        Take me to the builder
-      </Link>
+      <div id='home-container'>
+        <div className='text-center'>
+          <p>Welcome to</p>
+          <h1>
+            Nav<span>Bam</span>
+          </h1>
+        </div>
+        <Link to={'builder'} className='btn'>
+          Take me to the builder
+        </Link>
+      </div>
     </div>
   );
 };
