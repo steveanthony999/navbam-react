648f4f4809d12b7332266a806299d180e540122b
Steven Woodward <steveanthony999@gmail.com>
2020-12-14 18:33:26 -0800

    added font and began css

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,9 +2,15 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <>
-      <Link to={'builder'}>Take me to the builder</Link>
-    </>
+    <div id='home'>
+      <p>Welcome to</p>
+      <h1>
+        Nav<span>Bam</span>
+      </h1>
+      <Link to={'builder'} className='btn'>
+        Take me to the builder
+      </Link>
+    </div>
   );
 };
 
