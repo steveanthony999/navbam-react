a9068196170187dc4534f8fe301ff4bb37c8a498
Steven Woodward <steveanthony999@gmail.com>
2020-12-15 17:33:58 -0800

    fixed nav background color / transparent problem

--- frontend/src/components/Builder.js
+++ frontend/src/components/Builder.js
@@ -1,50 +1,64 @@
 import { useState } from 'react';
 import Navbar from './Navbar';
 import { ChromePicker } from 'react-color';
-import { Radio, useRadioState } from 'pretty-checkbox-react';
+import { Radio } from 'pretty-checkbox-react';
 import '@djthoms/pretty-checkbox';
 
 const Builder = () => {
-  const radio = useRadioState();
+  const [radio, setRadio] = useState();
 
-  const [bgColor, setBgColor] = useState({
-    background: '#ffffff',
+  const [colors, setColors] = useState({
+    background: '#cccccc',
+    navBackground: '#333333',
+    linkColor: '#ffffff',
+  });
+
+  const [storedColors, setStoredColors] = useState({
     navBackground: '#333333',
   });
+
   const [colorPicker, setColorPicker] = useState(false);
 
   const style = {
-    background: bgColor.background,
+    background: colors.background,
   };
 
-  const handleBgColorChange = (color) => {
-    if (radio.state === 'bgColor') {
-      setBgColor({ ...bgColor, background: color.hex });
-    } else if (radio.state === 'navBgColor') {
-      setBgColor({ ...bgColor, navBackground: color.hex });
+  const handleColorChange = (color) => {
+    if (radio === 'bgColor') {
+      setColors({ ...colors, background: color.hex });
+    } else if (radio === 'navBgColor') {
+      setColors({ ...colors, navBackground: color.hex });
+    } else if (radio === 'linkColor') {
+      setColors({ ...colors, linkColor: color.hex });
     }
   };
 
   const handleClick = (e) => {
-    if (e === 'bgColor') {
-      setColorPicker(true);
-    } else if (e === 'navBgColor') {
+    if (e) {
       setColorPicker(true);
     } else {
-      console.log('nada');
+      console.log('no');
     }
   };
 
-  const handleClose = (e) => {
-    if (e === 'bgColor') {
-      setColorPicker(false);
-    } else if (e === 'navBgColor') {
-      setColorPicker(false);
+  const handleClose = () => {
+    setColorPicker(false);
+  };
+
+  const handleRadio = (x) => {
+    setRadio(x);
+    if (x === 'transparentNavBg') {
+      setStoredColors({ ...colors, navBackground: colors.navBackground });
+      setColors({ ...colors, navBackground: 'transparent' });
+    } else if (x === 'navBgColor') {
+      setColors({ ...colors, navBackground: storedColors.navBackground });
     }
   };
 
   const popover = {
     position: 'absolute',
+    bottom: '0',
+    right: '0',
     zIndex: '2',
   };
 
@@ -57,41 +71,95 @@ const Builder = () => {
   };
 
   return (
-    <div style={style} id='builder'>
-      <Navbar navBg={bgColor.navBackground} />
-      {/* Change Background Color */}
-      <button className='circle-btn' onClick={() => handleClick(radio.state)} />
-      {colorPicker ? (
-        <div style={popover}>
-          <div style={cover} onClick={() => handleClose(radio.state)} />
-          <ChromePicker color={bgColor.background} onChange={handleBgColorChange} disableAlpha={true} />
+    <>
+      <Navbar navBg={colors.navBackground} linkColor={colors.linkColor} />
+      <div style={style} id='builder'>
+        <div id='attributes-window'>
+          {/* HEADER */}
+          {/* HEADER */}
+          {/* HEADER */}
+          <div id='attributes-header'>
+            <h2>Designer</h2>
+          </div>
+          {/* BODY */}
+          {/* BODY */}
+          {/* BODY */}
+          <div id='attributes-body'>
+            <Radio
+              name='component'
+              value='bgColor'
+              shape='round'
+              variant='fill'
+              animation='jelly'
+              color='info'
+              bigger
+              onChange={() => handleRadio('bgColor')}
+            >
+              Page Background Color
+            </Radio>
+            <Radio
+              name='component'
+              value='navBgColor'
+              shape='round'
+              variant='fill'
+              animation='jelly'
+              color='info'
+              bigger
+              onChange={() => handleRadio('navBgColor')}
+            >
+              Navbar Background Color
+            </Radio>
+            <Radio
+              name='component'
+              value='transparentNavBg'
+              shape='round'
+              variant='fill'
+              animation='jelly'
+              color='info'
+              bigger
+              onChange={() => handleRadio('transparentNavBg')}
+            >
+              Transparent Navbar
+            </Radio>
+            <Radio
+              name='component'
+              value='linkColor'
+              shape='round'
+              variant='fill'
+              animation='jelly'
+              color='info'
+              bigger
+              onChange={() => handleRadio('linkColor')}
+            >
+              Links Color
+            </Radio>
+          </div>
+          {/* FOOTER */}
+          {/* FOOTER */}
+          {/* FOOTER */}
+          <div id='attributes-footer'>
+            <div></div>
+            <button className='circle-btn' onClick={() => handleClick(radio)} />
+            {colorPicker ? (
+              <div style={popover}>
+                <div style={cover} onClick={handleClose} />
+                <ChromePicker
+                  color={
+                    radio === 'bgColor'
+                      ? colors.background
+                      : radio === 'navBgColor'
+                      ? colors.navBackground
+                      : colors.linkColor
+                  }
+                  onChange={handleColorChange}
+                  disableAlpha={true}
+                />
+              </div>
+            ) : null}
+          </div>
         </div>
-      ) : null}
-      <Radio
-        name='component'
-        value='bgColor'
-        shape='square'
-        variant='fill'
-        animation='jelly'
-        color='info'
-        bigger
-        {...radio}
-      >
-        Background Color
-      </Radio>
-      <Radio
-        name='component'
-        value='navBgColor'
-        shape='square'
-        variant='fill'
-        animation='jelly'
-        color='info'
-        bigger
-        {...radio}
-      >
-        Navbar Background Color
-      </Radio>
-    </div>
+      </div>
+    </>
   );
 };
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,39 @@
 import { Link } from 'react-router-dom';
 
-const Navbar = ({ navBg }) => {
-  const style = {
+const Navbar = ({ navBg, linkColor }) => {
+  const navStyle = {
     background: navBg,
   };
 
+  const linkStyle = {
+    color: linkColor,
+  };
+
   return (
-    <nav id='navbar' style={style}>
+    <nav id='navbar' style={navStyle}>
       <div id='nav-container'>
         <div id='nav-logo'>
-          <Link to='/'>
+          <Link to='/' style={linkStyle}>
             Nav<span>Bam</span>
           </Link>
         </div>
       </div>
       <div id='nav-links'>
-        <Link>About</Link>
-        <Link>Blog</Link>
-        <Link>Pricing</Link>
-        <Link>Contact</Link>
-        <Link>Shop</Link>
+        <Link to='/about' style={linkStyle}>
+          About
+        </Link>
+        <Link to='/blog' style={linkStyle}>
+          Blog
+        </Link>
+        <Link to='/pricing' style={linkStyle}>
+          Pricing
+        </Link>
+        <Link to='/contact' style={linkStyle}>
+          Contact
+        </Link>
+        <Link to='/shop' style={linkStyle}>
+          Shop
+        </Link>
       </div>
     </nav>
   );
