45bef092a0b639cf603ea1b34a012f641aa6df25
Steven Woodward <steveanthony999@gmail.com>
2020-12-22 23:52:02 -0800

    added sorry screen for mobile users

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,9 +10,13 @@ const Home = () => {
             Nav<span>Bam</span>
           </h1>
         </div>
-        <Link to={'builder'} className='btn'>
+        <Link to={'builder'} className='btn' id='builder-button'>
           Take me to the builder
         </Link>
+        <div id='builder-info' className='text-center'>
+          <h4>:,(</h4>
+          <p>Unfortunately this builder is not optimized for mobile. Please visit on a tablet or computer.</p>
+        </div>
       </div>
     </header>
   );
